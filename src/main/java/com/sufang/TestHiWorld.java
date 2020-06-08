5697d95f8f3268d7572e7b48b89f5abfae89642d
ltc <446770964@qq.com>
2020-06-08 10:37:43 +0800

    test

---
+++ src/main/java/com/sufang/TestHiWorld.java
@@ -0,0 +1,16 @@
+package com.sufang;
+
+/**
+ * @ClassName TestHiWorld
+ * @Description: TODO
+ * @Author ltc
+ * @Date 2020/6/8
+ * @Version V1.0
+ **/
+public class TestHiWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi World");
+    }
+}
